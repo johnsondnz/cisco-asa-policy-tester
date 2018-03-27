8704b65db0ac0df5eefea070fac9999717c22986
Donald Johnson <donald@it-ninja.xyz>
2018-03-27 13:22:45 +1300

    added skipped plays, resolution of names to IPs, custom hostfile and DNS support

--- tester.py
+++ tester.py
@@ -29,6 +29,7 @@ from logzero import logger
 from classes.structuredata import ASAPolicyTest
 from classes.report import GenerateReport
 from classes.search import RecursiveSearch
+from classes.resolve import Resolve, Lookup
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
@@ -55,6 +56,9 @@ def CheckArgs(args=None):
     parser.add_argument('-e', '--enable_password', action='store_true', dest='enable_password',
                         help='hidden password prompt')
 
+    parser.add_argument('-hf', '--hostfile', required=False,
+                        help='absolute path to hostfile')
+
     results = parser.parse_args(args)
 
     if results.password:
@@ -71,7 +75,8 @@ def CheckArgs(args=None):
         results.yaml,
         results.username,
         password,
-        enable_password
+        enable_password,
+        results.hostfile
     )
 
 
@@ -86,70 +91,104 @@ def ExecuteTests(action, context):
     elif action == 'drop':
         logger.info('Starting tests that should [FAIL] block')
 
-    for test_data in context[action]:
+    for index, test_data in enumerate(context[action]):
 
         logger.info('Processing {} protocol policy'.format(
             test_data['protocol'].lower()))
+
+        # check that provided IP fields are IPs, if not attempt to resolve
+        if hostfile_status == True:
+            source_ip = Lookup(test_data['source_ip'], hostfile_status, hostfile_list)
+            destination_ip = Lookup(test_data['destination_ip'], hostfile_status, hostfile_list)
+        else:
+            source_ip = Lookup(test_data['source_ip'], hostfile_status)
+            destination_ip = Lookup(test_data['destination_ip'], hostfile_status)
+        source_ip = source_ip.get_ip()
+        destination_ip = destination_ip.get_ip()
 
         # start the test
         if re.match(r'(udp|tcp)', str(test_data['protocol'].lower())):
 
             command = 'packet-tracer input {} {} {} {} {} {} detail'.format(
-                interface, test_data['protocol'], test_data['source_ip'], test_data['source_port'], test_data['destination_ip'], test_data['destination_port'])
+                interface, test_data['protocol'], source_ip['ip_address'], test_data['source_port'], destination_ip['ip_address'], test_data['destination_port'])
 
         elif 'icmp' in str(test_data['protocol'].lower()):
 
             command = 'packet-tracer input {} {} {} {} {} {} detail'.format(
-                interface, test_data['protocol'], test_data['source_ip'], test_data['icmp_type'], test_data['icmp_code'], test_data['destination_ip'])
+                interface, test_data['protocol'], source_ip['ip_address'], test_data['icmp_type'], test_data['icmp_code'], destination_ip['ip_address'])
 
-        logger.info(
-            'Processing: {} test, "{}"'.format(action, command))
 
         # Send the command to the ASA
-        cli_output = connect.send_command(command)
-        ParseData = ASAPolicyTest(script_dir, cli_output)
-        test_results = ParseData.TestResult()
+        # only if the source and destination IPs are valid
+        if source_ip['result'] != False and destination_ip['result'] != False:
 
-        # log to terminal the overall ASA action
-        logger.info('ASA reports: {}'.format(
-            test_results['action']))
+            logger.info(
+                'Processing: {} test, "{}"'.format(action, command))
 
-        # if NAT is reported log to terminal
-        if test_results['nat_rule'] is not None:
-            logger.debug('NAT detected: {} to {}'.format(test_results['nat_from'], test_results['nat_to']))
-            logger.debug('NAT rule: "{}"'.format(test_results['nat_rule']))
+            cli_output = connect.send_command(command)
+            ParseData = ASAPolicyTest(script_dir, cli_output)
+            test_results = ParseData.TestResult()
 
-        # log to terminal the drop reason if it exists
-        if test_results['drop_reason'] is not None:
-            logger.info('Drop reason: {}'.format(
-                test_results['drop_reason']))
+            # log to terminal the overall ASA action
+            logger.info('ASA reports: {}'.format(
+                test_results['action']))
 
-        # log to terminal the test result
-        if test_results['action'] == action:
-            logger.info('Test passed!\n')
-            grade = '[PASS]'
+            # if NAT is reported log to terminal
+            if test_results['nat_rule'] is not None:
+                logger.debug('NAT detected: {} to {}'.format(test_results['nat_from'], test_results['nat_to']))
+                logger.debug('NAT rule: "{}"'.format(test_results['nat_rule']))
+
+            # log to terminal the drop reason if it exists
+            if test_results['drop_reason'] is not None:
+                logger.info('Drop reason: {}'.format(
+                    test_results['drop_reason']))
+
+            # log to terminal the test result
+            if test_results['action'] == action:
+                logger.info('Test passed!\n')
+                grade = '[PASS]'
+            else:
+                logger.error('Test failed!\n')
+                grade = '[FAIL]'
+
+            testing_results.append({
+                'command': command,
+                'interface': interface,
+                'protocol': test_data['protocol'],
+                'source_ip': source_ip['ip_address'],
+                'source_port': test_data['source_port'] if test_data['source_port'] is not None else '',
+                'icmp_type': test_data['icmp_type'] if test_data['icmp_type'] is not None else '',
+                'icmp_code': test_data['icmp_code'] if test_data['icmp_code'] is not None else '',
+                'destination_ip': destination_ip['ip_address'],
+                'destination_port': test_data['destination_port'] if test_data['destination_port'] is not None else '',
+                'action': test_results['action'] if test_results['action'] is not None else '',
+                'expected_result': action,
+                'drop_reason': test_results['drop_reason'] if test_results['drop_reason'] is not None else '',
+                'nat_from': test_results['nat_from'] if test_results['nat_from'] is not None else '',
+                'nat_to': test_results['nat_to'] if test_results['nat_to'] is not None else '',
+                'nat_rule': test_results['nat_rule'] if test_results['nat_rule'] is not None else '',
+                'grade': grade
+            })
         else:
-            logger.error('Test failed!\n')
-            grade = '[FAIL]'
-
-        testing_results.append({
-            'command': command,
-            'interface': interface,
-            'protocol': test_data['protocol'],
-            'source_ip': test_data['source_ip'],
-            'source_port': test_data['source_port'] if test_data['source_port'] is not None else '',
-            'icmp_type': test_data['icmp_type'] if test_data['icmp_type'] is not None else '',
-            'icmp_code': test_data['icmp_code'] if test_data['icmp_code'] is not None else '',
-            'destination_ip': test_data['destination_ip'],
-            'destination_port': test_data['destination_port'] if test_data['destination_port'] is not None else '',
-            'action': test_results['action'],
-            'expected_result': action,
-            'drop_reason': test_results['drop_reason'] if test_results['drop_reason'] is not None else '',
-            'nat_from': test_results['nat_from'] if test_results['nat_from'] is not None else '',
-            'nat_to': test_results['nat_to'] if test_results['nat_to'] is not None else '',
-            'nat_rule': test_results['nat_rule'] if test_results['nat_rule'] is not None else '',
-            'grade': grade
-        })
+            testing_results.append({
+                'command': command,
+                'interface': interface,
+                'protocol': test_data['protocol'],
+                'source_ip': source_ip['ip_address'],
+                'source_port': test_data['source_port'] if test_data['source_port'] is not None else '',
+                'icmp_type': test_data['icmp_type'] if test_data['icmp_type'] is not None else '',
+                'icmp_code': test_data['icmp_code'] if test_data['icmp_code'] is not None else '',
+                'destination_ip': destination_ip['ip_address'],
+                'destination_port': test_data['destination_port'] if test_data['destination_port'] is not None else '',
+                'action': '',
+                'expected_result': action,
+                'drop_reason': '',
+                'nat_from': '',
+                'nat_to': '',
+                'nat_rule': '',
+                'grade': '[SKIP]'
+            })
+            logger.error('Skipping test record "{}" for interface "{}", invalid addresses detected\n'.format(index, interface))
 
     return testing_results
 
@@ -157,7 +196,7 @@ def ExecuteTests(action, context):
 if __name__ == "__main__":
 
     # capture the passed arguments
-    HOST, SSH_PORT, YAML_FILE, USERNAME, PASSWORD, ENABLE_PASSWORD = CheckArgs(
+    HOST, SSH_PORT, YAML_FILE, USERNAME, PASSWORD, ENABLE_PASSWORD, HOSTFILE = CheckArgs(
         sys.argv[1:])
 
     # construct the devi
@@ -171,42 +210,57 @@ if __name__ == "__main__":
         'verbose': False
     }
 
+    # load the hostfile to a list
+    try:
+        hostfile_list = Resolve()
+        hostfile_list = hostfile_list.load_hostfile(HOSTFILE)
+        logger.info('Hostfile "{}" found and loaded'.format(HOSTFILE))
+        hostfile_status = True
+    except Exception:
+        logger.error('Hostfile not loaded')
+        hostfile_status = False
+        pass
+
     # store the yaml tests
     with open('{}/tests/{}'.format(script_dir, YAML_FILE), 'r') as yml:
         test_map = yaml.safe_load(yml)
 
-    try:
-        logger.info('Attempting connection to {}'.format(device['ip']))
-        connect = ConnectHandler(**device)
-        if connect:
+    # try:
+    logger.info('Attempting connection to {}'.format(device['ip']))
+    connect = ConnectHandler(**device)
+    if connect:
 
-            logger.info('Connected to {}\n'.format(device['ip']))
+        logger.info('Connected to {}\n'.format(device['ip']))
 
-            results = {}
-            # setup each interface dict
-            for interface, test in test_map.items():
-                results[interface] = {}
+        results = {}
+        # setup each interface dict
+        for interface, test in test_map.items():
+            results[interface] = {}
 
-            # Begin the tests that should pass
-            for interface, test in test_map.items():
+        # Begin the tests that should pass
+        for interface, test in test_map.items():
 
-                logger.info('Processing {} tests'.format(interface))
+            logger.info('Processing {} tests'.format(interface))
 
-                # run each interfaces test and assign to dictionary
+            # run each interfaces test and assign to dictionary
+            if RecursiveSearch(test, 'allow'):
                 results[interface]['should_allow'] = ExecuteTests('allow', test)
+
+            if RecursiveSearch(test, 'drop'):
                 results[interface]['should_drop'] = ExecuteTests('drop', test)
 
-            GenerateReport(results, script_dir, datetime.datetime.now().strftime(
-                "%d/%m/%Y @ %H:%M:%S"))
+        print(results)
+        GenerateReport(results, script_dir, datetime.datetime.now().strftime(
+            "%d/%m/%Y @ %H:%M:%S"))
 
-            # check for FAIL items and generate the retest YAML
-            # save times with remediate rulesets
-            # uses a recusive search to look for grade: [FAIL] in result dict
-            # retest = RecursiveSearch(results, 'grade', '[FAIL]')
+        # check for FAIL items and generate the retest YAML
+        # save times with remediate rulesets
+        # uses a recusive search to look for grade: [FAIL] in result dict
+        # retest = RecursiveSearch(results, 'grade', '[FAIL]')
 
-            # if retest == True:
-            #     ReTest(results, script_dir)
+        # if retest == True:
+        #     ReTest(results, script_dir)
 
-    except:
-        logger.error('{}: {}'.format(
-            sys.exc_info()[0], sys.exc_info()[1]))
+    # except Exception:
+    #     logger.error('{}: {}'.format(
+    #         sys.exc_info()[0], sys.exc_info()[1]))
