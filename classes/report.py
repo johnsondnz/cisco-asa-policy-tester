b6799bc8dd4c8c31fb09d50f2a0ffec9bf1d8e26
Donald Johnson <donald@it-ninja.xyz>
2018-04-06 09:32:55 +1200

    started cleanup tasks, commented out for now

--- classes/report.py
+++ classes/report.py
@@ -2,23 +2,17 @@
 from jinja2 import Environment, FileSystemLoader
 from logzero import logger
 from os import unlink
+from os.path import isfile
+from glob import glob
 
 
 class GenerateReport(object):
 
     def __init__(self, context, script_dir, generated, reportname):
 
-        reportname = reportname if reportname != None else 'html_report'
-
-        cleanup = ['html_report.html', '{}.html'.format(reportname)]
-
-        for report in cleanup:
-            try:
-                unlink('{}/reports/{}'.format(script_dir, report))
-            except Exception:
-                logger.error(
-                    'Unable to cleanup: {}/reports/{}'.format(script_dir, report))
-                pass
+        self.script_dir = script_dir
+        self.reportname = reportname if reportname != None else 'html_report'
+        # self._cleanup()
 
         # setup the Jinja2 environment conditions
         file_loader = FileSystemLoader(
@@ -29,9 +23,36 @@ class GenerateReport(object):
         # Render template and print generated config to console
         template = env.get_template('report.j2')
         HTML = template.render(context=context, generated=generated)
-        with open('{}/reports/{}.html'.format(script_dir, reportname), 'w') as f:
+        with open('{}/reports/{}.html'.format(script_dir, self.reportname), 'w') as f:
             f.write(HTML)
         f.close()
 
         logger.info(
-            'HTML report output to "{}/reports/{}.html"'.format(script_dir, reportname))
+            'HTML report output to "{}/reports/{}.html"'.format(script_dir, self.reportname))
+
+    # def _cleanup(self):
+    #     '''
+    #     Method to remove old reports based on flags
+    #     '''
+
+    #     if self.reportname == 'html_report':
+    #         logger.debug('Cleaning up all previous reports')
+    #         for report in glob(u'*.html'):
+    #             filename = '{}/reports/{}'.format(self.script_dir, report)
+    #             try:
+    #                 if isfile(filename):
+    #                     unlink(filename)
+    #             except Exception:
+    #                 logger.error('Unable to cleanup: {}'.format(filename))
+    #                 pass
+    #     else:
+    #         cleanup = ['html_report.html', '{}.html'.format(self.reportname)]
+
+    #         for report in cleanup:
+    #             filename = '{}/reports/{}'.format(self.script_dir, report)
+    #             try:
+    #                 if isfile(filename):
+    #                     unlink(filename)
+    #             except Exception:
+    #                 logger.error('Unable to cleanup: {}'.format(filename))
+    #                 pass
